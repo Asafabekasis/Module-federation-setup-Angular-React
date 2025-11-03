ce81b1e7a925bcab12d9d9b7396f373d7af45270
asaf abek <asaf.abek@gmail.com>
2025-11-03 09:28:28 +0200

    added origin restrictions 3000 (react) 4200 (angular host) 61799 (angular remote)

--- angular to angular/client/webpack.config.js
+++ angular to angular/client/webpack.config.js
@@ -51,5 +51,41 @@ module.exports = {
         },
       }
     })
-  ]
+  ],
+  devServer: {
+    port: 61799,
+    headers: {
+      'Access-Control-Allow-Origin': 'http://localhost:4200, http://localhost:3000, http://localhost:61799',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+      'Access-Control-Allow-Credentials': 'true',
+    },
+    setupMiddlewares: (middlewares, devServer) => {
+      devServer.app.use((req, res, next) => {
+        const origin = req.headers.origin;
+        const referer = req.headers.referer;
+        const allowedOrigins = ['http://localhost:4200', 'http://localhost:3000','http://localhost:61799'];
+        
+        // Allow direct browser access (no origin header)
+        if (!origin && !referer) {
+          return next();
+        }
+        
+        // Block requests from origins other than allowed origins
+        if (origin && !allowedOrigins.includes(origin)) {
+          console.log(`❌ Blocked request from origin: ${origin}`);
+          return res.status(403).send('Access forbidden: Origin not allowed');
+        }
+        
+        if (referer && !allowedOrigins.some(allowed => referer.startsWith(allowed))) {
+          console.log(`❌ Blocked request from referer: ${referer}`);
+          return res.status(403).send('Access forbidden: Referer not allowed');
+        }
+        
+        next();
+      });
+      
+      return middlewares;
+    },
+  }
 };
